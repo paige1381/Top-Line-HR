908b178bb79be750c6e1847fc75827c236f7ad70
Paige Boyer <paige1381@gmail.com>
2018-03-12 19:59:00 -0400

    frontend solutions and contact pages done

--- public/app.js
+++ public/app.js
@@ -16,6 +16,30 @@ app.controller('BlogController', ['$http', function($http) {
 
 }]);
 
+app.controller('ContactController', ['$http', function($http) {
+
+}]);
+
+app.controller('AboutController', ['$http', function($http) {
+
+}]);
+
+app.controller('CoachingController', ['$http', function($http) {
+
+}]);
+
+app.controller('TeamsController', ['$http', function($http) {
+
+}]);
+
+app.controller('SalesController', ['$http', function($http) {
+
+}]);
+
+app.controller('HRController', ['$http', function($http) {
+
+}]);
+
 
 
 app.config(['$routeProvider','$locationProvider', function($routeProvider, $locationProvider) {
@@ -39,6 +63,41 @@ app.config(['$routeProvider','$locationProvider', function($routeProvider, $loca
     controllerAs: 'ctrl'
   });
 
+  $routeProvider.when('/contact', {
+    templateUrl: 'contact.html',
+    controller: 'ContactController',
+    controllerAs: 'ctrl'
+  });
+
+  $routeProvider.when('/about', {
+    templateUrl: 'about.html',
+    controller: 'AboutController',
+    controllerAs: 'ctrl'
+  });
+
+  $routeProvider.when('/coaching', {
+    templateUrl: 'coaching.html',
+    controller: 'CoachingController',
+    controllerAs: 'ctrl'
+  });
+
+  $routeProvider.when('/teams', {
+    templateUrl: 'teams.html',
+    controller: 'TeamsController',
+    controllerAs: 'ctrl'
+  });
+
+  $routeProvider.when('/sales', {
+    templateUrl: 'sales.html',
+    controller: 'SalesController',
+    controllerAs: 'ctrl'
+  });
+
+  $routeProvider.when('/hr', {
+    templateUrl: 'hr.html',
+    controller: 'HRController',
+    controllerAs: 'ctrl'
+  });
 
 
   $routeProvider.otherwise({
