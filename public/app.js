a682edfd29fd5ab92f49ef9dbe9d51422575260d
Paige Boyer <paige1381@gmail.com>
2018-03-12 11:51:07 -0400

    added media section

--- public/app.js
+++ public/app.js
@@ -4,17 +4,21 @@ app.controller('MenuController', ['$http', function($http) {
   this.mobileMenu = false;
 }]);
 
+app.controller('HomeController', ['$http', function($http) {
+
+}])
+
 app.config(['$routeProvider','$locationProvider', function($routeProvider, $locationProvider) {
   $locationProvider.html5Mode({ enabled: true });
 
-  // $routeProvider.when('/home', {
-  //   templateUrl: 'home.html',
-  //   controller: 'HomeController',
-  //   controllerAs: 'ctrl'
-  // });
-  //
-  // $routeProvider.otherwise({
-  //   redirectTo: '/home'
-  // })
+  $routeProvider.when('/home', {
+    templateUrl: 'home.html',
+    controller: 'HomeController',
+    controllerAs: 'ctrl'
+  });
+
+  $routeProvider.otherwise({
+    redirectTo: '/home'
+  })
 
 }])
